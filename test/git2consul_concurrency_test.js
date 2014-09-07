aaaf3c7fca43dedd2175052bcf192559ef80db01
Ryan Breen <rbreen@vistaprint.com>
2014-09-07 00:02:36 -0400

    More test cleanup.

--- test/git2consul_concurrency_test.js
+++ test/git2consul_concurrency_test.js
@@ -14,61 +14,49 @@ var git_commands = require('../lib/utils/git_commands.js');
 describe('Concurrency protections', function() {
 
   var massive_concurrency_test = function(normal_ref_order) {
-  
+
     var my_gm;
 
     var sample_key = 'sample_key';
     var sample_value = 'test data';
-  
+
     var default_repo_config = git_utils.createConfig().repos[0];
 
     return function(done) {
-      
-      git_utils.addFileToGitRepo(sample_key, sample_value, "Concurrency test.", function(err) {
-        git_manager.createGitManager(default_repo_config, function(err, gm) {
-          if (err) return done(err);
-        
-          my_gm = gm;
-    
-          sample_value = 'new value';
-    
-          git_utils.addFileToGitRepo(sample_key, sample_value, "Concurrency update.", function(err) {
+
+      git_utils.addFileToGitRepo(sample_key, sample_value, "Concurrency update.", false, function(err) {
+        if (err) return done(err);
+
+        git_commands.getCurrentRef(git_utils.TEST_REMOTE_REPO, function(err, ref) {
+          if (err) done(err);
+
+          var first_ref = ref;
+
+          var sample_key2 = 'sample_key2';
+          var sample_value2 = 'test data2';
+
+          git_utils.addFileToGitRepo(sample_key2, sample_value2, "Second file for concurrencty test.", false, function(err) {
             if (err) return done(err);
-      
+
             git_commands.getCurrentRef(git_utils.TEST_REMOTE_REPO, function(err, ref) {
               if (err) done(err);
-      
-              var first_ref = ref;
-      
-              var sample_key2 = 'sample_key2';
-              var sample_value2 = 'test data2';
-      
-              git_utils.addFileToGitRepo(sample_key2, sample_value2, "Second file for concurrencty test.", function(err) {
+
+              var second_ref = ref;
+              var bm = git_utils.GM.getBranchManager('master');
+
+              bm.handleRefChange((normal_ref_order ? first_ref : second_ref), function(cb) {
                 if (err) return done(err);
-    
-                git_commands.getCurrentRef(git_utils.TEST_REMOTE_REPO, function(err, ref) {
-                  if (err) done(err);
-      
-                  var second_ref = ref;
-                  var bm = my_gm.getBranchManager('master');
-              
-                  bm.handleRefChange((normal_ref_order ? first_ref : second_ref), function(cb) {
+              });
+
+              bm.handleRefChange((normal_ref_order ? second_ref : first_ref), function(cb) {
+                if (err) return done(err);
+                // At this point, the git_manager should have populated consul with both sample_key
+                consul_utils.validateValue('/' + default_repo_config.name + '/master/' + sample_key, sample_value, function(err, value) {
+                  if (err) return done(err);
+
+                  consul_utils.validateValue('/' + default_repo_config.name + '/master/' + sample_key2, sample_value2, function(err, value) {
                     if (err) return done(err);
-                  });
-          
-                  bm.handleRefChange((normal_ref_order ? second_ref : first_ref), function(cb) {
-                    if (err) return done(err);
-                    // At this point, the git_manager should have populated consul with both sample_key
-                    consul_utils.getValue('/' + default_repo_config.name + '/master/' + sample_key, function(err, value) {
-                      if (err) return done(err);
-                      value.should.equal(sample_value);
-                
-                      consul_utils.getValue('/' + default_repo_config.name + '/master/' + sample_key2, function(err, value) {
-                        if (err) return done(err);
-                        value.should.equal(sample_value2);
-                        done();
-                      });
-                    });
+                    done();
                   });
                 });
               });
@@ -78,7 +66,7 @@ describe('Concurrency protections', function() {
       });
     };
   };
-  
+
   it ('should serialize access to a single branch', massive_concurrency_test(true));
   it ('should serialize access to a single branch even if updates are out of order', massive_concurrency_test(false));
 });
