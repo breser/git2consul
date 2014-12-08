e902d08e93a167575a4ae1ac8f4a050fe91bb966
Ryan Breen <rbreen@vistaprint.com>
2014-12-08 17:30:19 -0500

    Restore graceful shutdown test.

--- test/git2consul_graceful_shutdown_test.js
+++ test/git2consul_graceful_shutdown_test.js
@@ -3,18 +3,18 @@ var should = require('should');
 // We want this above any git2consul module to make sure logging gets configured
 require('./git2consul_bootstrap_test.js');
 
-var git_manager = require('../lib/git_manager.js');
+var git = require('../lib/git');
 
 var git_utils = require('./utils/git_utils.js');
 
 describe('Graceful Shutdown', function() {
   
   before(function() {
-    git_manager.mock();
+    git.mock();
   });
 
   it ('should successfully fire if nothing else is happening', function(done) {
-    git_manager.gracefulShutdown(function() {
+    git.gracefulShutdown(function() {
       done();
     });
   });
@@ -29,13 +29,13 @@ describe('Graceful Shutdown', function() {
     git_utils.addFileToGitRepo(sample_key, sample_value, "Update a file.", false, function(err) {
       if (err) return done(err);
 
-      git_utils.GM.getBranchManager('master').handleRefChange(0, function(err) {
+      git_utils.repo.branches['master'].handleRefChange(0, function(err) {
         if (err) return done(err);
          shutdown_seen.should.equal(false);
       });
 
       // This should not fire until after we've processed the handleRefChange above
-      git_manager.gracefulShutdown(function() {
+      git.gracefulShutdown(function() {
         shutdown_seen = true;
         done();
       });
@@ -43,7 +43,7 @@ describe('Graceful Shutdown', function() {
   });
 
   after(function() {
-    git_manager.mock();
+    git.mock();
   });
 
 });
