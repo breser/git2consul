547863ec1864540199e2a29a517dd79d88599251
Ryan Breen <rbreen@vistaprint.com>
2014-12-13 23:10:28 -0500

    Fix and simplify hook tests.

--- test/git2consul_webhook_and_polling_test.js
+++ test/git2consul_webhook_and_polling_test.js
@@ -8,6 +8,7 @@ var bootstrap = require('./git2consul_bootstrap_test.js');
 var consul_utils = require('./utils/consul_utils.js');
 
 var Repo = require('../lib/git/repo.js');
+var git_commands = require('../lib/git/commands.js');
 var git_utils = require('./utils/git_utils.js');
 
 var logger = require('../lib/logging.js');
@@ -44,26 +45,32 @@ var logger = require('../lib/logging.js');
 
   describe('webhook config validation', function() {
 
-    var my_hooked_gm;
-
     it ('should reject invalid webhook config', function(done) {
-      var repo_config = git_utils.createRepoConfig();
-      repo_config.hooks = hook_config;
 
-      var repo = new Repo(repo_config);
-      repo.init(function(err) {
-        if (hook_config[0]) {
-          err[0].should.equal(hook_config[0].err);
-        } else {
-          err[0].should.startWith('Hook configuration failed due to');
-        }
-        done();
+      git_commands.init(git_utils.TEST_REMOTE_REPO, function(err) {
+        if (err) return done(err);
+
+        // When we create a repo, we need it to have an initial commit.  The call to addFile provides that.
+        git_utils.addFileToGitRepo("readme.md", "Stub file to give us something to commit.", "Init webhook config repo.", function(err) {
+          if (err) return done(err);
+
+          var repo_config = git_utils.createRepoConfig();
+          repo_config.hooks = hook_config;
+
+          var repo = new Repo(repo_config);
+          repo.init(function(err) {
+            if (hook_config[0]) {
+              err[0].should.equal(hook_config[0].err);
+            } else {
+              err[0].should.startWith('Hook configuration failed due to');
+            }
+            done();
+          });
+        });
       });
     });
   });
 });
-
-var repo_counter = 0;
 
 [
   // Test webhooks sharing a port
@@ -91,114 +98,113 @@ var repo_counter = 0;
   [{
     'type': 'github',
     'url': '/githubpoke_bogus_branch',
-    'port': 5252,
+    'port': 5253,
     'body': { ref: "refs/heads/bogus_branch", head_commit: {id: 12345} },
-    'fqurl': 'http://localhost:5252/githubpoke_bogus_branch',
+    'fqurl': 'http://localhost:5253/githubpoke_bogus_branch',
     'no_change_expected': true
   },{
     'type': 'stash',
     'url': '/stashpoke_bogus_branch',
-    'port': 5252,
+    'port': 5253,
     'body': { refChanges: [{refId: "refs/heads/bogus_branch", toHash: "0"}]},
-    'fqurl': 'http://localhost:5252/stashpoke_bogus_branch',
+    'fqurl': 'http://localhost:5253/stashpoke_bogus_branch',
     'no_change_expected': true
   },{
     'type': 'bitbucket',
     'url': '/bitbucket_bogus_branch',
-    'port': 5252,
+    'port': 5253,
     'body': '%7B%22repository%22%3A+%7B%22website%22%3A+null%2C+%22fork%22%3A+false%2C+%22name%22%3A+%22configuration%22%2C+%22scm%22%3A+%22git%22%2C+%22owner%22%3A+%22ryanbreen%22%2C+%22absolute_url%22%3A+%22%2Fryanbreen%2Fconfiguration%2F%22%2C+%22slug%22%3A+%22configuration%22%2C+%22is_private%22%3A+true%7D%2C+%22truncated%22%3A+false%2C+%22commits%22%3A+%5B%7B%22node%22%3A+%226f086f3d3de7%22%2C+%22files%22%3A+%5B%7B%22type%22%3A+%22modified%22%2C+%22file%22%3A+%22jobs_service%2Fauspice%2Fdemo-consumer-key%22%7D%5D%2C+%22raw_author%22%3A+%22Ryan+Breen+%3Crbreen%40vistaprint.com%3E%22%2C+%22utctimestamp%22%3A+%222014-10-18+23%3A20%3A47%2B00%3A00%22%2C+%22author%22%3A+%22Ryan+Breen%22%2C+%22timestamp%22%3A+%222014-10-19+01%3A20%3A47%22%2C+%22raw_node%22%3A+%226f086f3d3de724b9007934408e023b628a59ea15%22%2C+%22parents%22%3A+%5B%2230c88d68d029%22%5D%2C+%22branch%22%3A+%22dev%22%2C+%22message%22%3A+%22Adding+bitbucket+test.%5Cn%22%2C+%22revision%22%3A+null%2C+%22size%22%3A+-1%7D%5D%2C+%22canon_url%22%3A+%22https%3A%2F%2Fbitbucket.org%22%2C+%22user%22%3A+%22ryanbreen%22%7D',
-    'fqurl': 'http://localhost:5252/bitbucket_bogus_branch',
+    'fqurl': 'http://localhost:5253/bitbucket_bogus_branch',
     'no_change_expected': true
   }],
   // Test no-op changes with non-HEAD refs
   [{
     'type': 'github',
     'url': '/githubpoke_bogus_ref',
-    'port': 5252,
+    'port': 5254,
     'body': { ref: "refs/remotes/origin/master", head_commit: {id: 12345} },
-    'fqurl': 'http://localhost:5252/githubpoke_bogus_branch',
+    'fqurl': 'http://localhost:5254/githubpoke_bogus_branch',
     'no_change_expected': true
   },{
     'type': 'stash',
     'url': '/stashpoke_bogus_ref',
-    'port': 5252,
+    'port': 5254,
     'body': { refChanges: [{refId: "refs/remotes/origin/master", toHash: "0"}]},
-    'fqurl': 'http://localhost:5252/stashpoke_bogus_ref',
+    'fqurl': 'http://localhost:5254/stashpoke_bogus_ref',
     'no_change_expected': true
   },{
     'type': 'bitbucket',
     'url': '/bitbucket_bogus_ref',
-    'port': 5252,
+    'port': 5254,
     'body': '',
-    'fqurl': 'http://localhost:5252/bitbucket_bogus_ref',
+    'fqurl': 'http://localhost:5254/bitbucket_bogus_ref',
     'no_change_expected': true
-  }]
+  }]  
 ].forEach(function(hook_config) {
+
+  var repo;
 
   describe('webhook', function() {
 
-    before(function(done) {
+    // Add a file for the config, commit it, and validate that it has populated properly.
+    var test_hook_req = function(config, cb) {
+      var sample_key = 'webhook_key_' + config.type;
+      var sample_value = config.type + ' test data';
 
-      // Enable manual mode.  We don't want the standard git2consul bootstrap tests to create a git_manager
-      // that is enabled without hooks as this just causes endless confusion.
-      bootstrap.manual_mode(true);
+      git_utils.addFileToGitRepo(sample_key, sample_value, "Webhook.", function(err) {
+        if (err) return cb(err);
 
-      bootstrap.cleanup(function(err) {
+        var req_conf = { url: config.fqurl, method: 'POST' };
+        if (config.type === 'bitbucket') {
+          req_conf.form = { payload: decodeURIComponent(config.body).replace(/\+/g, ' ') };
+        } else req_conf.json = config.body
+        
+        if (config.type === 'stash') req_conf.headers = {'content-encoding':'UTF-8'};
+        request(req_conf, function(err) {
 
-        if (err) return done(err);
+          if (err) return cb(err);
 
-        var repo_config = git_utils.createRepoConfig();
-        repo_config.hooks = hook_config;
-        repo_config.name = "webhook_test" + repo_counter;
-        ++repo_counter;
+          // If this is a test that won't trigger an update, such as a req specifying an untracked branch,
+          // short-circuit here and don't test for a KV update.
+          if (config.no_change_expected) return cb();
 
-        git_utils.initRepo(repo_config, function(err, gm) {
-          if (err) return done(err);
-          done();
-        });
+          consul_utils.waitForValue('test_repo/master/' + sample_key, function(err) {
+            if (err) return cb(err);
 
-      });
-    });
-
-    var sample_data_randomizer = 0;
-
-    // This creates a function suitable as the predicate of a mocha test.  The function will enclose
-    // the config object and use it to send a request to the webhook and validate the response.
-    var create_request_validator = function(config) {
-      return function(done) {
-        var repo_name = git_utils.repo.name;
-        var sample_key = 'sample_key';
-        var sample_value = 'stash test data ' + sample_data_randomizer;
-        ++sample_data_randomizer;
-        git_utils.addFileToGitRepo(sample_key, sample_value, "Webhook.", false, function(err) {
-          if (err) return done(err);
-
-          var req_conf = { url: config.fqurl, method: 'POST' };
-          if (config.type === 'bitbucket') {
-            req_conf.form = { payload: decodeURIComponent(config.body).replace(/\+/g, ' ') };
-          } else req_conf.json = config.body
-          
-          if (config.type === 'stash') req_conf.headers = {'content-encoding':'UTF-8'};
-          request(req_conf, function(err) {
-
-            if (err) return done(err);
-
-            // If this is a test that won't trigger an update, such as a req specifying an untracked branch,
-            // short-circuit here and don't test for a KV update.
-            if (config.no_change_expected) return done();
-
-            consul_utils.waitForValue(repo_name + '/master/sample_key', function(err) {
-              if (err) return done(err);
-
-              done();
-            });
+            cb();
           });
         });
-      };
+      });
     };
 
-    hook_config.forEach(function(config) {
-      it (config.type + ' should handle inbound requests', create_request_validator(config));
+    it ('should handle inbound requests', function(done) {
+      var repo_config = git_utils.createRepoConfig();
+      repo_config.hooks = hook_config;
+
+      git_utils.initRepo(repo_config, function(err, repo) {
+        if (err) return done(err);
+
+        // Handle the initial sync of this repo.  Init adds a file to the remote repo, and this line syncs
+        // that to our local cache and to consul.
+        repo.branches['master'].handleRefChange(0, function(err) {
+          if (err) return done(err);
+
+          // Test each hook config, 1 at a time.  Since they are updating the same repo, having all webhooks
+          // fire in parallel would lead to undefined results.
+          var test_config = function() {
+            config = hook_config.pop();
+            console.log('Running test with hook:\n%s', require('util').inspect(config));
+            test_hook_req(config, function(err) {
+              if (err) return done(err);
+
+              if (hook_config.length > 0) return test_config();
+              done();
+            });
+          };
+
+          test_config();
+        });
+      });
     });
   });
 });
@@ -218,14 +224,11 @@ var repo_counter = 0;
 
   describe('polling config validation', function() {
 
-    var my_hooked_gm;
-
     it ('should reject invalid polling config', function(done) {
       var repo_config = git_utils.createRepoConfig();
       repo_config.hooks = hook_config;
 
-      var repo = new Repo(repo_config);
-      repo.init(function(err) {
+      git_utils.initRepo(repo_config, function(err, repo) {
         if (hook_config[0]) {
           err[0].should.equal('Hook configuration failed due to Polling intervals must be positive integers');
         } else {
@@ -239,41 +242,27 @@ var repo_counter = 0;
 
 describe('polling hook', function() {
 
-  before(function(done) {
-
-    // Enable manual mode.  We don't want the standard git2consul bootstrap tests to create a git_manager
-    // that is enabled without hooks as this just causes endless confusion.
-    bootstrap.manual_mode(true);
-
-    bootstrap.cleanup(function(err) {
-
-      if (err) return done(err);
-
-      var repo_config = git_utils.createRepoConfig();
-      repo_config.hooks = [{
-        'type': 'polling',
-        'interval': '1'
-      }];
-      repo_config.name = "polling_test";
-
-      git_utils.initRepo(repo_config, function(err, gm) {
-        if (err) return done(err);
-        done();
-      });
-
-    });
-  });
-
   it ('should handle polling updates', function(done) {
-    var repo_name = git_utils.repo.name;
-    var sample_key = 'sample_key';
-    var sample_value = 'stash test data';
-    git_utils.addFileToGitRepo(sample_key, sample_value, "Webhook.", false, function(err) {
+
+    var repo_config = git_utils.createRepoConfig();
+    repo_config.hooks = [{
+      'type': 'polling',
+      'interval': '1'
+    }];
+    repo_config.name = "polling_test";
+
+    git_utils.initRepo(repo_config, function(err, repo) {
       if (err) return done(err);
 
-      consul_utils.waitForValue(repo_name + '/master/sample_key', function(err) {
+      var sample_key = 'sample_key';
+      var sample_value = 'stash test data';
+      git_utils.addFileToGitRepo(sample_key, sample_value, "Polling hook.", function(err) {
         if (err) return done(err);
-        done();
+
+        consul_utils.waitForValue('polling_test/master/sample_key', function(err) {
+          if (err) return done(err);
+          done();
+        });
       });
     });
   });
