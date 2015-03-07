ff3f619484e2acaa3114c3f439e95ee8fa9072ec
Ryan Breen <rbreen@cimpress.com>
2015-03-06 21:05:56 -0500

    Add integration test repo population.

--- integration/features/git2consul.feature
+++ integration/features/git2consul.feature
@@ -1,7 +1,19 @@
 @git2consul
 
 Feature: Ensure that proper infrastructure for the tests is setup
-  Vagrant server boxes should be properly spun up and running
 
-  Scenario: Consul cluster is running
-    Then The /consulserver[1-3]{1}/ box is running
+  Scenario: All servers are running
+    Given The git integration repo is initialized
+    Given The consulserver1 box is online
+    Given The consulserver2 box is online
+    Given The consulserver3 box is online
+    Given The consulserver1 box has a git2consul config
+
+  Scenario Outline: All servers are running git2consul
+    Then The <box_name> box is running git2consul
+    Then The <box_name> box has 2 known peers
+    Examples:
+      | box_name        |
+      | consulserver1   |
+      | consulserver2   |
+      | consulserver3   |
--- integration/features/step_definitions/git2consul_steps.rb
+++ integration/features/step_definitions/git2consul_steps.rb
@@ -11,21 +11,44 @@ def run_command(cmd)
   end
 end
 
-Given /The (.*) box is running/ do |servers|
-  system("vagrant up #{servers} --parallel --provision")
+def write_file(path, body)
+  File.open(path, 'w') { |file| file.write(body) }
 end
 
-Then /The (.*) service is running on (.*) (.*) box/ do |service, box_name, os|
-  out = run_command("vagrant ssh -c \"service #{service} status\" #{box_name}")
+Given /The git integration repo is initialized/ do
+  FileUtils.rm_rf 'integration_test_repo'
+  Dir.mkdir 'integration_test_repo'
+  Dir.chdir 'integration_test_repo' do
+    system("git init")
+    ['dev','test','prod'].each { |env|
+      system("git checkout -b #{env}")
+      write_file("readme.md", "#{env} readme")
+      system("git add readme.md")
+      system("git commit -m \"Initial commit to #{env}\"")
+    }
+  end
+end
+
+Given /The (.*) box is online/ do |server|
+  system("vagrant up #{server}")
+  #system("vagrant provision #{server}")
+end
+
+Then /The (.*) box has a git2consul config/ do |server|
+  req = Net::HTTP::Put.new('/v1/kv/git2consul/config', initheader = { 'Content-Type' => 'application/json'})
+  req.body = File.open("config.json", "rb").read
+  puts "Sending #{req.body} to #{server}"
+  response = Net::HTTP.new(server, 8500).start {|http| http.request(req) }
+  puts response.code
+end
+
+Then /The (.*) box is running git2consul/ do |box_name|
+  run_command("vagrant ssh -c \"sudo service git2consul stop ; sudo rm -rf /tmp/git_cache && sudo service git2consul start\" #{box_name}")
+  out = run_command("vagrant ssh -c \"service git2consul status\" #{box_name}")
   expect(out).to include("running")
 end
 
-Then /The (.*) server has (.*) known peers/ do |server, num_peers|
+Then /The (.*) box has 2 known peers/ do |server|
   out = run_command("vagrant ssh -c \"consul info\" #{server}")
-  expect(out).to include("num_peers = #{num_peers}")
-end
-
-Then /The box (.*) \((.*)\) has the file (.*)/ do |server, os, file|
-  out = run_command("vagrant ssh -c 'ls #{file}' #{server}")
-  expect(out).to include(file)
+  expect(out).to include("num_peers = 2")
 end
