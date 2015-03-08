bbca9cfc40bd365a3136c43f924104ea8ede6371
Ryan Breen <rbreen@cimpress.com>
2015-03-07 21:20:40 -0500

    Change order of init steps to make behavior more predicatble.

--- integration/features/git2consul.feature
+++ integration/features/git2consul.feature
@@ -3,10 +3,10 @@
 Feature: Ensure that proper infrastructure for the tests is setup
 
   Scenario: All servers are running
-    Given The git integration repo is initialized
     Given The consulserver1 box is online
     Given The consulserver2 box is online
     Given The consulserver3 box is online
+    Given The git integration repo is initialized
     Given The consulserver1 box has a git2consul config
 
   Scenario Outline: All servers are running git2consul
--- integration/features/step_definitions/git2consul_steps.rb
+++ integration/features/step_definitions/git2consul_steps.rb
@@ -28,7 +28,8 @@ Given /The git integration repo is initialized/ do
     system("git init")
     http = Net::HTTP.new("consulserver1", 8500)
     # Make sure to purge any existing git2consul results from the KV
-    http.request(Net::HTTP::Delete.new("/v1/kv/integration?recurse"))
+    http.request(Net::HTTP::Delete.new("/v1/kv/integration?recurse")).code
+    system("curl http://consulserver1:8500/v1/kv/integration?recurse&pretty=true")
     commit_file("readme.md", "stubby", "stub commit to master")
     ['dev','test','prod'].each { |env|
       system("git checkout -b #{env}")
@@ -41,17 +42,19 @@ end
 Given /The (.*) box is online/ do |server|
   system("vagrant up #{server}")
   system("vagrant provision #{server}") if ENV.has_key? 'VAGRANT_REPROVISION'
+
+  # Stop a running git2consul process and delete the existing cache dir
+  puts run_command("vagrant ssh -c \"sudo service git2consul stop ; sudo rm -rf /tmp/git_cache\" #{server}")
 end
 
 Then /The (.*) box has a git2consul config/ do |server|
   req = Net::HTTP::Put.new('/v1/kv/git2consul/config', initheader = { 'Content-Type' => 'application/json'})
   req.body = File.open("config.json", "rb").read
   response = Net::HTTP.new(server, 8500).start {|http| http.request(req) }
-  puts response.code
 end
 
 Then /The (.*) box is running git2consul/ do |box_name|
-  run_command("vagrant ssh -c \"sudo service git2consul stop ; sudo rm -rf /tmp/git_cache && sudo service git2consul start\" #{box_name}")
+  run_command("vagrant ssh -c \"sudo service git2consul start\" #{box_name}")
   sleep 1
   out = run_command("vagrant ssh -c \"service git2consul status\" #{box_name}")
   expect(out).to include("running")
