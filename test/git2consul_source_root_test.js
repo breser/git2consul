da63bc50794517fbab31086c34eebc6281aa0f5a
Ryan Breen <rbreen@cimpress.com>
2015-11-02 21:40:07 -0500

    Trim trailing slashes from source_root config.

--- test/git2consul_source_root_test.js
+++ test/git2consul_source_root_test.js
@@ -46,4 +46,32 @@ describe('KV source_root', function() {
     });
   });
 
+  it ('should support setting a custom source_root for repos', function(done) {
+
+    // Create a remote git repo.  Then, init a Repo object with include_branch_name disabled and validate
+    // that files are in the appropriate place in the Consul KV store.
+    git_commands.init(git_utils.TEST_REMOTE_REPO, function(err) {
+      if (err) return done(err);
+
+      mkdirp(git_utils.TEST_REMOTE_REPO + "nested/enough/for/my/purposes", function(cb) {
+        if (err) return done(err);
+
+        git_utils.addFileToGitRepo("nested/enough/for/my/purposes/readme.md", "Test file beneath source_root", "Test commit.", function(err) {
+          if (err) return done(err);
+
+          var repo_config = git_utils.createRepoConfig();
+          repo_config.source_root = "nested/enough/";
+          var repo = new Repo(repo_config);
+          repo.init(function(err) {
+            if (err) return done(err);
+            consul_utils.validateValue('test_repo/master/for/my/purposes/readme.md', "Test file beneath source_root", function(err, value) {
+              if (err) return done(err);
+              done();
+            });
+          });
+        });
+      });
+    });
+  });
+
 });
