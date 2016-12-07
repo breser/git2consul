f1d8c305ce9618d7c5025c2b3f5df5346988e7c0
Ben Webber <benjamin.webber@gmail.com>
2016-12-07 15:09:15 +0800

    Test ignore_file_extension behaviour with known/unknown file types

--- test/git2consul_ignore_file_extension_test.js
+++ test/git2consul_ignore_file_extension_test.js
@@ -33,6 +33,19 @@ describe('ignore_file_extension', function() {
     done();
   });
 
+  var test_files_known_extensions = {
+    'ignore_file_extension/config_yaml.yaml': 'foo: bar',
+    'ignore_file_extension/config_yml.yml': 'foo: bar',
+    'ignore_file_extension/config_json.json': '{"foo": "bar"}',
+    'ignore_file_extension/config_properties.properties': 'foo=bar',
+  };
+
+  var test_files_unknown_extensions = {
+    'ignore_file_extension/config_ini.ini': 'foo = bar',
+    'ignore_file_extension/config_toml.toml': 'foo = bar',
+  };
+
+
   it ('should ignore file extension when expand_keys == true', function(done) {
 
     // Initialize a Repo and commit a properties file. Validate that the
@@ -87,4 +100,69 @@ describe('ignore_file_extension', function() {
     });
   });
 
+  for (var filename in test_files_known_extensions) {
+    (function(filename) {
+      var contents = test_files_known_extensions[filename];
+      var key_name = filename.substr(0, filename.lastIndexOf('.'));
+      var extension = filename.substr(filename.lastIndexOf('.') + 1);
+      it ('should remove file extension for known file type: ' + extension, function(done) {
+
+        git_commands.init(git_utils.TEST_REMOTE_REPO, function(err) {
+          if (err) return done(err);
+
+          mkdirp(git_utils.TEST_REMOTE_REPO + 'ignore_file_extension', function(cb) {
+            if (err) return done(err);
+
+            git_utils.addFileToGitRepo(filename, contents, 'Test data added: ' + filename, function(err) {
+              if (err) return done(err);
+
+              repo_config.expand_keys = true;
+              var repo = new Repo(repo_config);
+
+              repo.init(function(err) {
+                if (err) return done(err);
+                consul_utils.validateValue('test_repo/master/' + key_name + '/foo', 'bar', function(err, value) {
+                  if (err) return done(err);
+                  done();
+                });
+              });
+            });
+          });
+        });
+      });
+    })(filename);
+  }
+
+  for (var filename in test_files_unknown_extensions) {
+    (function(filename) {
+      var contents = test_files_unknown_extensions[filename];
+      var key_name = filename.substr(0, filename.lastIndexOf('.'));
+      var extension = filename.substr(filename.lastIndexOf('.') + 1);
+      it ('should not remove file extension for unknown file type: ' + extension, function(done) {
+
+        git_commands.init(git_utils.TEST_REMOTE_REPO, function(err) {
+          if (err) return done(err);
+
+          mkdirp(git_utils.TEST_REMOTE_REPO + 'ignore_file_extension', function(cb) {
+            if (err) return done(err);
+
+            git_utils.addFileToGitRepo(filename, contents, 'Test data added: ' + filename, function(err) {
+              if (err) return done(err);
+
+              repo_config.expand_keys = true;
+              var repo = new Repo(repo_config);
+
+              repo.init(function(err) {
+                if (err) return done(err);
+                consul_utils.validateValue('test_repo/master/' + key_name + '/foo', '', function(err, value) {
+                  if (err) return done(err);
+                  done();
+                });
+              });
+            });
+          });
+        });
+      });
+    })(filename);
+  }
 });
