e6f490223ce640635b738d0155c0f5a5c9fa188d
Reser, Ben <Ben.Reser@cdk.com>
2017-08-11 20:32:45 -0700

    Re-enable test coverage reporting
    Fixed the cov command just like I'd done for the test command, so
    that it will properly exit with an non-zero exit code on test failure.
    
    Switched back to using the cov script for doing the tests.

--- test/test.sh
+++ test/test.sh
@@ -2,4 +2,12 @@
 
 set -o pipefail
 
-./node_modules/.bin/_mocha --reporter spec -t 10000 test | bunyan
+bin=$(npm bin)
+mocha_args='--reporter spec -t 10000 test'
+
+if [ "$1" == "cov" ]; then
+  "$bin/istanbul" cover --root . -x node_modules -x test --dir ./reports \
+    "$bin/_mocha" -- $mocha_args | bunyan
+else
+  "$bin/_mocha" $mocha_args | bunyan
+fi
